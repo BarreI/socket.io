f2b8775fbe1abed48706c22211f83d1d21da3365
AntiMelee <tmskiti@outlook.jp>
2021-04-21 11:10:02 +0900

    chat base

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const PORT = process.env.PORT || 8000;
 
-app.get('/' , function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/main.html')
 });
 
-io.on('connection', function(socket){
-  socket.on('message',function(msg) {
-    console.log("message:" + msg);
-  })
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('message', msg);
+  });
 })
 
-http.listen(PORT, function(){
+http.listen(PORT, function () {
   console.log('server listening. Port:' + PORT);
 });
-
-//git にあげれてないよ
